62d15c77a9df0fbecd40a443e7f53699ae4ee75c
kutencat <jasmine02133210g@gmail.com>
2022-03-21 23:10:38 +0900

    3 files modified, 1 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
+import { Product } from '../shared/product.model';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products = products;
+  products: Product = {
+    name: 'taro',
+    price: 400,
+    description: 'hogehoge',
+  };
 
   share() {
     window.alert('The product has been shared!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
